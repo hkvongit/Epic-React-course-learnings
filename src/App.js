fb97ae11c5bdd8e2016c3e62dca90f58da16cf23
hkvongit <45459914+hkvongit@users.noreply.github.com>
2021-05-27 23:33:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useRef, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [lowerCasedValue, setLowerCasedValue] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const value = event.target.value;
+    setLowerCasedValue(value.toLowerCase());
+  };
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">user name</label>
+        <input
+          id="name"
+          onChange={(e) => onChange(e)}
+          value={lowerCasedValue}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
